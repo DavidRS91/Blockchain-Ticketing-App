07964ad863493f577504f3762bbb22fad2412613
David Scott <david.r.scott91@gmail.com>
2018-03-12 16:36:05 -0700

    rollback version of web3

--- test/Event.test.js
+++ test/Event.test.js
@@ -3,7 +3,6 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
-const { INFURA_KEY } = require("../config");
 
 const compiledGenerator = require("../ethereum/build/EventGenerator.json");
 const compiledEvent = require("../ethereum/build/Event.json");
@@ -35,7 +34,6 @@ beforeEach(async () => {
 
 describe("Events", () => {
   it("deploys generator and event", () => {
-    console.log(INFURA_KEY);
     assert.ok(generator.options.address);
     assert.ok(event.options.address);
   });
